8c4c11b4417f0c73df0e9b4fddfa3571ee3a39af
BlankPenguin <55719433+BlankPenguin@users.noreply.github.com>
2019-10-14 22:24:31 -0400

    Add files via upload
    Another one.

---
+++ P4T1b_Navarro.py
@@ -0,0 +1,52 @@
+# This turtle program is going to show my initials. The first section is going to
+# do the A and the second section is the N. 
+# 10-14-2019
+# CTI-110 P4T2 - Bug Collector
+# Alexis Navarro
+
+import turtle
+
+turtle.pencolor('red')
+turtle.pensize(5)
+turtle.dot()
+turtle.left(85)     #
+turtle.forward(100) #
+turtle.right(10)    # This section is for my first initial.
+turtle.forward(100) #
+turtle.right(90)    #
+turtle.forward(20)  #
+turtle.right(75)    #
+turtle.forward(200) #
+turtle.penup()      #
+turtle.right(90)    #
+turtle.forward(15)  #
+
+turtle.right(90)
+turtle.forward(100)
+turtle.pendown()
+turtle.left(90)
+turtle.forward(25)
+turtle.right(180)
+turtle.forward(40)
+
+
+turtle.penup()
+turtle.forward(200)
+turtle.right(100)
+turtle.forward(100) # This section is for the second initial.
+turtle.right(85)    #
+turtle.forward(100)
+turtle.pendown()
+turtle.right(95)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(75)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(20)
+turtle.left(75)
+turtle.forward(200)
+turtle.right(90)
+turtle.forward(20)
+
